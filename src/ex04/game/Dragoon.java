105fc0e77a931111f85f277aebcd5436b1858a39
KJcording <rmfdnjf98@gmil.com>
2025-12-08 17:53:31 +0900

    상속 및 해시까지

--- src/ex04/game/Dragoon.java
+++
@@ -1,6 +0,0 @@
-package ex04.game;
-
-public class Dragoon extends Protoss{
-    int hp = 100;
-    int attack = 5;
-}
--- src/ex04/game/Zealot.java
+++
@@ -1,6 +0,0 @@
-package ex04.game;
-
-public class Zealot extends Protoss{
-    int hp = 100;
-    int attack = 10;
-}
